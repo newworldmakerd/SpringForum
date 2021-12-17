2f06daa1c6ee0a90cb469d8eb45215d70592b5cb
lyh <1371850283@qq.com>
2021-12-17 22:47:58 +0800

    first commit

---
+++ src/main/java/com/example/forum_test/ForumTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.forum_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forum_test/ForumTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forum_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
